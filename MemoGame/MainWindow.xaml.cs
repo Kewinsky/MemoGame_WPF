3f2b8de2cfc2b82f3285e0b6a3f6a3a19b71bca7
Kewin Tao Anh <kewin.taoanh@gmail.com>
2022-04-13 22:17:43 +0200

    dodanie obslugi klikania ikon

--- MemoGame/MainWindow.xaml.cs
+++ MemoGame/MainWindow.xaml.cs
@@ -38,7 +38,7 @@ namespace MemoGame
                 "🐻", "🐻",
                 "🦒", "🦒",
                 "🐺", "🐺",
-                "🐼", "🐼",
+                "🦏", "🦏",
                 "🦍", "🦍"
             };
 
@@ -54,6 +54,32 @@ namespace MemoGame
                 textBlock.Text = nextEmoji;
                 animalEmoji.RemoveAt(index);
 
+            }
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+        private int matchesFound;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
             }
         }
     }
